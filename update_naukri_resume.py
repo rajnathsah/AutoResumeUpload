8b49172ede32d053e401eed1caf6f592e95ff3f5
Raj Nath Sah <raj4u4all@gmail.com>
2019-07-29 21:40:59 +0400

    added config file and batch file to run

--- update_naukri_resume.py
+++ update_naukri_resume.py
@@ -3,7 +3,9 @@ from selenium.webdriver.chrome.options import Options
 import time
 import os
 import logging
+import configparser
 
+# Generate log file name
 logfile = r'update_naukri_profile_{}.log'.format(time.strftime('%Y_%m_%d_%H.%M.%S'))
 updateimage = r'update_naukri_profile_{}.png'.format(time.strftime('%Y_%m_%d_%H.%M.%S'))    
 logging.basicConfig(filename=logfile, filemode='w', format='%(asctime)s,%(msecs)d %(levelname)-8s [%(filename)s:%(lineno)d] %(message)s',datefmt='%Y-%m-%d:%H:%M:%S',level=logging.DEBUG)
@@ -14,12 +16,32 @@ logger.info('Start')
 opts=Options()
 opts.headless=False
 
+# read config file for parameters
+try:
+    config=configparser.ConfigParser()
+    config.read('config.ini')
+    username=config['login']['username']
+    password=config['login']['password']
+    resume=config['resume']['filename']
+except Exception as ex:
+    logger.error('Error in reading configuration file: {}'.format(ex))
+
+#read resume file and validate path
 cur_dir_path=os.getcwd()
 logger.info('Current working directory - {}'.format(cur_dir_path))
-path_to_cv=os.path.join(cur_dir_path,"<resume>")
-logger.info('Path to cv- {}'.format(path_to_cv))
+path_to_resume=os.path.join(cur_dir_path,resume)
+try:
+    if os.path.isfile(path_to_resume):
+        pass
+    else:
+        raise
+except Exception as ex:
+    logger.error('Error in locating resume file : {}'.format(path_to_resume))
+    raise
+    
+logger.info('Path to cv- {}'.format(path_to_resume))
 
-#assert opts.headless
+# open browser, login and upload resume
 try:
     browser=Chrome(options=opts,executable_path=r'chromedriver.exe')
     browser.get('https://www.naukri.com/nlogin/login?URL=http://www.naukri.com/mnjuser/profile?id=&altresid')
@@ -28,24 +50,27 @@ try:
     logger.info('Open url in chrome')
     time.sleep(5)    
     
-    username = browser.find_element_by_id('usernameField')
-    username.send_keys('<username>')
-    logger.info('Enter username')
+    usernameField = browser.find_element_by_id("usernameField")    
+    usernameField.send_keys(username)
+    logger.info('Entered username')
     
-    password=browser.find_element_by_id('passwordField')
-    password.send_keys('<password>')
-    logger.info('Enter password')
+    passwordField=browser.find_element_by_id('passwordField')
+    passwordField.send_keys(password)
+    logger.info('Entered password')
     
     clickbtn=browser.find_element_by_xpath("//*[@id='loginForm']/div[5]/div/button").click()
     time.sleep(5)
     logger.info('Click login and wait for 5 sec')
     
-    browser.find_element_by_id('attachCV').send_keys(path_to_cv)
-    time.sleep(5)
-    browser.save_screenshot(updateimage)
-    logger.info('Upload cv and wait for 5 sec')
-    
-    browser.quit()
+    try:
+        browser.find_element_by_id('attachCV').send_keys(path_to_resume)
+        time.sleep(5)
+        browser.save_screenshot(updateimage)
+        logger.info('Upload cv and wait for 5 sec')
+        browser.quit()
+    except Exception as ex:
+        logger.error('Error in login and cv upload: {}'.format(ex))
+            
 except Exception as ex:
     print(ex)
     logger.error('Error in cv upload- {}'.format(ex))
